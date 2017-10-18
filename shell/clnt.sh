d60b14e91c41019cd929da032c65206d3aff2575
dillip <dillippradhan1988@gmail.com>
2017-10-18 11:50:43 +0530

    f

--- shell/clnt.sh
+++ shell/clnt.sh
@@ -2,16 +2,9 @@
 ssh -t -t root@172.17.0.2 <<EOF 
     cd /var/www/localhost/htdocs/jenkinsci
     #git pull origin master    
-    timeStamp=$(date +%s)
-    randomHash=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-    uniqueId="$timeStamp$randomHash"
-    fileExt=".txt"
-    uniqueFileName=$uniqueId$fileExt
-
-    echo $uniqueFileName
-    touch $uniqueFileName
-    find . -name \*.php -exec php -l {} \; | grep -v 'No syntax errors' > $uniqueFileName
-    noOfLine=$(wc -l < $uniqueFileName)                                         
+    touch phpError.txt
+    find . -name \*.php -exec php -l {} \; | grep -v 'No syntax errors' > phpError.txt
+    noOfLine=$(wc -l < phpError.txt)                                             
    
     echo $noOfLine
     if [ $? -eq 1 && $noOfLine -gt 0]
