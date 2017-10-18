da16600bc4cf9f121024863571a43a02c6734a7c
dillip <dillippradhan1988@gmail.com>
2017-10-18 11:32:29 +0530

    f

--- shell/clnt.sh
+++ shell/clnt.sh
@@ -2,9 +2,16 @@
 ssh -t -t root@172.17.0.2 <<EOF 
     cd /var/www/localhost/htdocs/jenkinsci
     #git pull origin master    
-    touch phpError.txt    
-    tt="$(find . -name \*.php -exec php -l {} \; | grep -v 'No syntax errors'  > /dev/null)"
-    noOfLine=$(wc -l < phpError.txt)                                             
+    timeStamp=$(date +%s)
+    randomHash=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+    uniqueId=$timeStamp$randomHash
+    fileExt=".txt"
+    uniqueFileName=$uniqueId$fileExt
+
+    echo $uniqueFileName
+    touch $uniqueFileName
+    find . -name \*.php -exec php -l {} \; | grep -v 'No syntax errors' > $uniqueFileName
+    noOfLine=$(wc -l < $uniqueFileName)                                         
    
     echo $noOfLine
     if [ $? -eq 1 && $noOfLine -gt 0]
